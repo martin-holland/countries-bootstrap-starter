2a2785bb00263fb8f7ba04296db53effc77b0ecb
Martin Holland <martin.holland@free.fi>
2023-02-05 14:54:22 +0200

    Lesson 1 commit

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,24 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
+import { Spinner } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
+import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
+import { initializeCountries } from "../features/countries/countriesSlice";
+const numFormatter = require('@skalwar/simple_number_formatter');
 
 const Countries = () => {
+  const dispatch = useDispatch();
+
+  const countriesList = useSelector((state) => state.countries.countries)
+  const loading = useSelector((state) => state.countries.isLoading)
   const [search, setSearch] = useState('')
 
-  console.log("Search: ", search)
+  useEffect(() => {
+    dispatch(initializeCountries())
+  }, [dispatch])
 
-  // We will be replacing this with data from our API.
-  const country = {
-    name: {
-      common: 'Example Country'
-    }
+  if (loading) {
+    return (
+      <Col className="text-center m-5">
+        <Spinner
+          animation="border"
+          role="status"
+          className="center"
+          variant="info"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </Col>
+    );
   }
 
   return (
@@ -38,16 +56,30 @@ const Countries = () => {
         </Col>
       </Row>
       <Row xs={2} md={3} lg={4} className=" g-3">
-            <Col className="mt-5">
+        {countriesList.filter((c) => {
+            return c.name.official
+              .toLowerCase()
+              .includes(search.toLowerCase());
+          }).map((country) => (<Col className="mt-5">
               <LinkContainer
                 to={`/countries/${country.name.common}`}
                 state={{ country: country }}
               >
                 <Card className="h-100">
+                <Card.Img
+                    variant="top"
+                    src={country.flags.svg}
+                    className="rounded h-50"
+                    style={{
+                      objectFit: 'cover',
+                      minHeight: '200px',
+                      maxHeight: '200px',
+                    }}
+                  />
                   <Card.Body className="d-flex flex-column">
-                    <Card.Title>{'Single Country Common Name'}</Card.Title>
+                    <Card.Title>{country.name.common}</Card.Title>
                     <Card.Subtitle className="mb-5 text-muted">
-                      {'Single Country Official Name'}
+                      {country.name.official}
                     </Card.Subtitle>
                     <ListGroup
                       variant="flush"
@@ -55,19 +87,24 @@ const Countries = () => {
                     >
                       <ListGroup.Item>
                         <i className="bi bi-translate me-2"></i>
+                        {Object.values(country.languages ?? {}).join(', ')}
                       </ListGroup.Item>
                       <ListGroup.Item>
                         <i className="bi bi-cash-coin me-2"></i>
+                        {Object.values(country.currencies || {})
+                          .map((currency) => currency.name)
+                          .join(', ')}
                       </ListGroup.Item>
-
                       <ListGroup.Item>
                         <i className="bi bi-people me-2"></i>
+                        {numFormatter(country.population)}
                       </ListGroup.Item>
                     </ListGroup>
                   </Card.Body>
                 </Card>
               </LinkContainer>
-            </Col>
+            </Col>))}
+            
       </Row>
     </Container>
   );
---
+++ src/features/countries/countriesSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit"
+import countryService from '../../services/countries'
+
+export const countriesSlice = createSlice({
+    name: 'countries',
+    initialState: {
+        countries: [],
+        isLoading: true
+    },
+    reducers: {
+        getCountries(state, action) {
+            state.countries = action.payload
+        },
+        isLoading(state, action) {
+            state.isLoading = action.payload
+        }
+    },
+})
+
+export const initializeCountries = () => {
+    return async (dispatch) => {
+        const countries = await countryService.getAll();
+        dispatch(getCountries(countries));
+        setTimeout(() => dispatch(isLoading(false)), 1000)
+        
+    }
+}
+
+export const {getCountries, isLoading} = countriesSlice.actions
+
+export default countriesSlice.reducer
